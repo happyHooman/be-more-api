a41c8f46071032d597faa5f2e115fd21916c91a4
Alex <tornea.alexandru@gmail.com>
2018-12-05 16:04:02 +0200

    updated get-badge to return multiple badges

--- badge/get-badge.js
+++ badge/get-badge.js
@@ -3,21 +3,38 @@ import {success, failure} from "../libs/response-lib";
 import tables from "../libs/tables";
 
 export async function main(event, context, callback) {
-	const params = {
-		TableName: tables.badges,
-		Key: {
-			badgeId: event.pathParameters.id
-		}
-	};
+	let idList = event.pathParameters.id.split(',');
 
-	try {
-		const result = await dynamoDbLib.call("get", params);
-		if (result.Item) {
-			callback(null, success(result.Item));
-		} else {
-			callback(null, failure({status: false, error: "Item not found."}));
+	if (idList.length > 1) {
+		const params = {
+			TableName: tables.badges
+		};
+
+		try {
+			const result = await dynamoDbLib.call("scan", params);
+			const badgesList = result.Items.filter(badge => idList.indexOf(badge.badgeId) > -1);
+			callback(null, success(badgesList));
+		} catch (e) {
+			callback(null, failure({status: false}));
 		}
-	} catch (e) {
-		callback(null, failure({status: false}));
+	} else {
+		const params = {
+			TableName: tables.badges,
+			Key: {
+				badgeId: idList[0]
+			}
+		};
+
+		try {
+			const result = await dynamoDbLib.call("get", params);
+			if (result.Item) {
+				callback(null, success([result.Item]));
+			} else {
+				callback(null, failure({status: false, error: "Item not found."}));
+			}
+		} catch (e) {
+			callback(null, failure({status: false}));
+		}
 	}
+
 }
